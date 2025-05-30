f32690949153a0b9257baab366d6accb70a2c561
lane15vasconcelos <lanevasconcellos1@gmail.com>
2025-05-30 11:03:06 -0400

    Update

---
+++ aplication/Program.java
@@ -0,0 +1,34 @@
+package aplication;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+
+        System.out.print("Name: ");
+        student.name = sc.nextLine();
+        student.grade1 = sc.nextDouble();
+        student.grade2 = sc.nextDouble();
+        student.grade3 = sc.nextDouble();
+
+        if (student.finalClass() >= 60 ) {
+            System.out.printf("Final Grade: %.2f% ", student.finalClass());
+            System.out.print("Pass");
+        }
+        else {
+            System.out.printf("Final Grade: %.2f%n", student.finalClass());
+            System.out.println("Failed");
+            System.out.printf("Mission %.2f points%n", student.missingPoint());
+        }
+
+
+    }
+
+    
+}
---
+++ entities/Student.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Student {
+    public String name;
+    public double grade1;
+    public double grade2;
+    public double grade3;
+
+    public double finalClass() {
+        return grade1 + grade2 + grade3;
+    }
+
+    public double missingPoint() {
+        if (finalClass() >= 60) {
+            return 0.0;
+        }
+        else {
+            return 60 - finalClass();
+        }
+    }
+
+}
